2f9b1d841857fdc32fef26a612ee5edba62c4838
Matt Gleason <mjgleason@foundationdevices.com>
2026-07-19 15:11:56 -0400

    fixed hyprland config after update

--- modules/hyprland/default.nix
+++ modules/hyprland/default.nix
@@ -1,6 +1,7 @@
 {config, inputs, ...}: {
   wayland.windowManager.hyprland = {
     enable = true;
+    configType = "hyprlang";
     extraConfig = builtins.readFile "${inputs.self}/modules/hyprland/hyprland.conf";
   };
 }
